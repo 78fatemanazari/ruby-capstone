1d88d942f0698bd34fc3c8ef26b3ed30839057d4
fatemanazari <fatemanazari78@gmail.com>
2023-11-07 22:12:29 +0330

    Add two properties to the book class

--- book.rb
+++ book.rb
@@ -1,11 +1,12 @@
 class Book < Item
-  attr_reader :title, :author, :cover_state
+  attr_reader :title, :author, :cover_state, :publisher
 
-  def initialize(id, publish_date, title, author, cover_state)
+  def initialize(id, publish_date, title, author, cover_state, publisher)
     super(id, publish_date)
     @title = title
     @author = author
     @cover_state = cover_state
+    @publisher = publisher
   end
 
   def can_be_archived?
