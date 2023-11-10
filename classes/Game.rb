dec35eba5c6dee9ae2221b4d280cdc5c557fe5ab
CobbyElsonfx <andohfrancis9187@gmail.com>
2023-11-10 11:09:56 +0000

    added author class and implemented methods

--- classes/Game.rb
+++ classes/Game.rb
@@ -5,7 +5,7 @@ class Game < Item
   attr_accessor :multiplayer, :last_played_at, :publish_date
 
   def initialize(multiplayer, last_played_at, publish_date)
-    super(published_date)
+    super(publish_date)
     @multiplayer = multiplayer
     @last_played_at = Date.parse(last_played_at)
   end
